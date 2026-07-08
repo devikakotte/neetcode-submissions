8c69d89ed64f6328efac19996b375c690a58d700
Devika <23wh1a05b7@bvrithyderabad.edu.in>
2026-07-08 07:40:27 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        /*Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i < n - 1;i++){
+            if(nums[i] == nums[i + 1]){
+                return true;
+            }
+        }
+        return false;*/
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        /*--------------BRUTE FORCE---------------
+        int n = nums.length;
+        for(int i = 0;i < n;i++){
+            for(int j = i + 1;j < n;j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;*/
+        /*---------------BETTER-----------
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i < n - 1;i++){
+            if(nums[i] == nums[i + 1]){
+                return true;
+            }
+        }
+        return false;*/
+        
+        //-----------OPTIMAL-----------//
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
